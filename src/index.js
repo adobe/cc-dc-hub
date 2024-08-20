f9ba36b882a8962c52d280d78fb19aa9ed84f717
nnguyen <nnguyen@adobe.com>
2024-08-20 13:46:03 -0700

    Fix routing issue with HashRouter and Links

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css';
-import Layout from './Layout';
-import { HomePage, ToolsPage, ToolsDetailPage, ContributionPage } from './pages';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout";
+import {
+  HomePage,
+  ToolsPage,
+  ToolsDetailPage,
+  ContributionPage,
+} from "./pages";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="/"  element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-            <Route path="tools" element={<ToolsPage />} />
-            <Route path="tools/:id" element={<ToolsDetailPage />} />
-            <Route path="contribution" element={<ContributionPage />} />
+          <Route path="tools" element={<ToolsPage />} />
+          <Route path="tools/:id" element={<ToolsDetailPage />} />
+          <Route path="contribution" element={<ContributionPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
 
     {/* <Home /> */}
     {/* <ToolsPage /> */}
