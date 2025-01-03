bd1b4c59edfb2517bd32cf17599af1830dd5ddf9
Doug Ross <dougross@adobe.com>
2025-01-02 16:04:44 -0800

    Adding banner styling as per legal

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -7,15 +7,15 @@ import { Banner } from "../../common";
 export const Footer = () => {
   return (
     <Ftr marginBottom="10px">
-      <View id="footer">
-        <View id="ResponsiveContainer">
-          <Banner type="hubFooter">
+      <Banner>
             This is a community-driven site where creatives and developers come together to 
             create some amazing tools and resources for the Adobe community. And while awesome, 
             please remember that these tools are developed and licensed by third parties and 
             are not officially supported – in any part – by Adobe. Issues should be logged 
             in the associated tool's repository, which can be found on the tool detail page.
-          </Banner>
+      </Banner>
+      <View id="footer">
+        <View id="ResponsiveContainer">
           <Flex
             width="100%"
             height="100%"
